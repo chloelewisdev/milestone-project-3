76c298af1ccedcf6a924507955ce0fe1a41b8295
chloelewisdev <baldwinchloe@hotmail.com>
2021-03-10 20:25:11 +0000

    Changes to Scroll to top button

--- static/js/script.js
+++ static/js/script.js
@@ -1,27 +1,30 @@
-  $(document).ready(function(){
-    
-    $('.sidenav').sidenav();
+// $('.sidenav').sidenav(); 
+//Only runs function when page is ready:
+$(document).ready(function() {
 
-    //Scroll to top button - code from https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_scroll_to_top
-    var mybutton = document.getElementById("myBtn");
+//Scroll to top button - code from https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_scroll_to_top
+var mybutton = document.getElementById("myBtn");
 
-    // When the user scrolls down 20px from the top of the document, the button shows
-    window.onscroll = function() {
-        scrollFunction();
-    };
+// When the user scrolls down 20px from the top of the document, the button shows
+window.onscroll = function() {
+    scrollFunction();
+};
 
-    function scrollFunction() {
-        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-            mybutton.style.display = "block";
-        } else {
-            mybutton.style.display = "none";
-        }
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
     }
+}
 
-    // When the user clicks on the button, scroll to the top of the document
-    function topFunction() {
-        document.body.scrollTop = 0;
-        document.documentElement.scrollTop = 0;
-    }
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
 
-    });
+// Closes mobile dropdown menu after item clicked - source: https://stackoverflow.com/questions/42401606/how-to-hide-collapsible-bootstrap-4-navbar-on-click        
+$('.navbar-nav>li>a').on('click', function() {
+    $('.navbar-collapse').collapse('hide');
+});
