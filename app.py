702c59d0f3fdcb6fb79dae50e9f25ffedacbc66c
chloelewisdev <baldwinchloe@hotmail.com>
2021-03-12 15:15:39 +0000

    debug set to false to complete the project

--- app.py
+++ app.py
@@ -190,4 +190,4 @@ def my_tips(username):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
